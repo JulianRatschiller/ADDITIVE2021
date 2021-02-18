b2a0f8e2b14f1f121a3d3f24f0ec9df65e72d36e
Julian Ratschiller <julian.ratschiller@gmail.com>
2021-02-18 13:54:16 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React from 'react'
 import "./App.css"
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props){ 
+    super(props) 
+    this.state = { email:'',name:'', lastname:''} 
+    this.handleChange = this.handleChange.bind(this) 
+    this.handleSubmit = this.handleSubmit.bind(this) 
+  } 
+
+  handleSubmit(event){ 
+    const { email, name, lastname } = this.state 
+    event.preventDefault() 
+    alert(` 
+      ____Your Details____\n 
+      Name : ${name} 
+      Lastname: ${lastname}
+      Email : ${email} 
+    `) 
+  } 
+
+  handleChange(event){ 
+    this.setState({ 
+      [event.target.name] : event.target.value 
+    }) 
+  } 
+
+
+  render() {
+    return (
     <>
     
     <div className="wrapper wrapper_left">
@@ -11,41 +38,47 @@ function App() {
       </div>
       </div>
       <div className="wrapper wrapper_right">
-      <form>
+     
       <div className="radio_buttons">
          <input className="radio_btn" type="radio" id="radio1" name="radios" required></input>
-         <label for="radio1"> Male&nbsp;&nbsp;</label>
+         <label htmlFor="radio1"> Male&nbsp;&nbsp;</label>
          <input className="radio_btn" type="radio" id="radio2" name="radios" required></input>
-         <label for="radio1"> Female&nbsp;&nbsp;</label>
+         <label htmlFor="radio1"> Female&nbsp;&nbsp;</label>
          <input className="radio_btn" type="radio" id="radio3" name="radios" required></input>
-         <label for="radio1"> Other&nbsp;&nbsp;</label>
+         <label htmlFor="radio1"> Other&nbsp;&nbsp;</label>
       </div>
-         
+      <form onSubmit={this.handleSubmit}>
       <div>
          <label >
            <p>Name</p>
-           <input className="input_box" name="name" placeholder="Your Name" required/>
+           <input type="text" value = {this.state.name} onChange={this.handleChange}
+           className="input_box" name="name" placeholder="Your Name" required/>
          </label>
          <label >
            <p>Lastname</p>
-           <input className="input_box" name="name" placeholder="Your Lastname" required/>
+           <input type="text" value = {this.state.lastname} onChange={this.handleChange}
+           className="input_box" name="lastname" placeholder="Your Lastname" required/>
          </label>
          <label > 
            <p>Email</p>
-           <input className="input_box" type="email" name="email" id="email" placeholder="Please enter your email" required>
+           <input type="text" value = {this.state.email} onChange={this.handleChange} 
+           className="input_box" type="email" name="email" id="email" placeholder="Please enter your email" required>
            </input>
          </label>
          <div className="checkbox">
          <input type="checkbox" id="check" name="check1" required></input>
-         <label for="check"> I agree with the privacy policies</label>
+         <label htmlFor="check"> I agree with the privacy policies</label>
            </div>
-           <div className="btn"><button type="submit">Subscribe</button></div>     
+           <div className="btn"><button type="submit" /*value={this.state.value}*/>Subscribe</button></div>     
        </div>
        
-      </form>
+     </form>
     </div>
+   
 </>
-  );
+    )
 }
+}
+
 
 export default App;
